0d2bc444e0f4b43a2bffe6f2efdd16659c94c9e4
Arunprasad Rajkumar <ar.arunprasad@gmail.com>
2024-09-27 07:34:14 +0000

    Add some description
    Signed-off-by: Arunprasad Rajkumar <ar.arunprasad@gmail.com>

--- cmd/root.go
+++ cmd/root.go
@@ -12,13 +12,9 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "pg-subscriber",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Reimplementation of Postgres logical replication in Go.",
+	Long: `pg-subscriber is a reimplementation of Postgres logical replication in
+	Go.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
